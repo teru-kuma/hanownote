042b8eccbf69a9648f3b411d68698b454c80c9b1
teru-kuma <162723208+teru-kuma@users.noreply.github.com>
2026-09-03 01:04:55 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+/* 変更したら index.html の APP_VERSION と同じ値に上げる（README「バージョン」参照） */
+const VERSION = "1.0.0";
+const CACHE = "kuchi-log-" + VERSION;
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener("message", e => {
+  if (e.data === "skipWaiting") self.skipWaiting();
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
